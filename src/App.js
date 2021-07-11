8f40abeed2290e36d3fc63792544cf6500f48961
FrederickMih <fredtimermih@gmail.com>
2021-07-11 18:52:04 +0100

    Linters setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Math Magician Setup</h1>
+        <h1>Math Magician Setup</h1>
       </header>
     </div>
   );
