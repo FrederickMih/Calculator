f1bee6d2c678766b96b94ef6c05fb14e1701f23a
FrederickMih <fredtimermih@gmail.com>
2021-08-05 02:49:45 +0100

    Add the third group of the ButtonPanel buttons(1, 2, 3, +)

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,20 +5,26 @@ function ButtonPanel() {
 // Should render the calculator panel with all Buttons
   return (
     <div>
-      <Button name="AC" />
-      <Button name="+/-" />
-      <Button name="%" />
-      <Button name="/" />
+      <div>
+        <Button name="AC" />
+        <Button name="+/-" />
+        <Button name="%" />
+        <Button name="/" />
+      </div>
+      <div>
+        <Button name="7" />
+        <Button name="8" />
+        <Button name="9" />
+        <Button name="X" />
+      </div>
+      <div>
+        <Button name="4" />
+        <Button name="5" />
+        <Button name="6" />
+        <Button name="-" />
+      </div>
+      {/* // <div></div> */}
     </div>
-    <div>
-      <Button name="7" />
-      <Button name="8" />
-      <Button name="9" />
-      <Button name="X" />
-    </div>
-  // <div></div>
-  // <div></div>
-
   );
 }
 
