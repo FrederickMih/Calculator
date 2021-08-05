74823ad51db0cb273b1d3a1e610fe8f7ce9a580b
FrederickMih <fredtimermih@gmail.com>
2021-08-05 02:44:28 +0100

    Add the first second group of the ButtonPanel buttons(7, 8, 9, X)

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -10,7 +10,12 @@ function ButtonPanel() {
       <Button name="%" />
       <Button name="/" />
     </div>
-  // <div></div>
+    <div>
+      <Button name="7" />
+      <Button name="8" />
+      <Button name="9" />
+      <Button name="X" />
+    </div>
   // <div></div>
   // <div></div>
 
