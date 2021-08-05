929a193048ac0decc6da3c63aad840371a90a91e
FrederickMih <fredtimermih@gmail.com>
2021-08-05 04:27:00 +0100

    Add total, next, operation props to the calDataObj

--- src/logic/calulate.js
+++ src/logic/calulate.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Big from 'big.js';
 
-function calculate(CalDataObj, BtnName) {
-
+function calculate(calDataObj, btnName) {
+  const { total, next, operation } = calDataObj;
 }
 
 export default calculate;
