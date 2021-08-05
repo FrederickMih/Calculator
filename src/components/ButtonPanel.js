552fad4227b13e33e4516c53b797bc1bddc710ba
FrederickMih <fredtimermih@gmail.com>
2021-08-05 02:39:34 +0100

    Add the first group of the ButtonPanel component( AC, +/-, %, ÷)

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from 'Button';
+//
+function ButtonPanel() {
+// Should render the calculator panel with all Buttons
+  return (
+    <div>
+      <Button name="AC" />
+      <Button name="+/-" />
+      <Button name="%" />
+      <Button name="/" />
+    </div>
+  // <div></div>
+  // <div></div>
+  // <div></div>
+
+  );
+}
+
+export default ButtonPanel;
