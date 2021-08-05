6ab68b0479bdb297adc6bb954cf08f718ba796f0
FrederickMih <fredtimermih@gmail.com>
2021-08-05 05:08:12 +0100

    Add an if statement to null calDateObj if btnName = (AC)

--- src/logic/calulate.js
+++ src/logic/calulate.js
@@ -1,12 +1,19 @@
+import { operation } from 'retry';
 import operate from ',/operate';
 
 function calculate(calDataObj, btnName) {
-  let { total, next } = calDataObj;
+  let { total, next, operation } = calDataObj;
   if (btnName === '+/-') {
     total *= -1;
     next *= -1;
   }
-  return { total, next };
+
+  if (btnName === 'AC') {
+    total = null;
+    next = null;
+    operation = null;
+  }
+  return { total, next, operation };
 }
 
 export default calculate;
