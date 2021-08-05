2eea22e74e47fdfbe09e9c21ef8bf2a7dd3916b3
FrederickMih <fredtimermih@gmail.com>
2021-08-05 16:40:07 +0100

    Add loop to check when operation === '-')

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,7 +1,9 @@
 import Big from 'big.js';
 
 const operate = (numberOne, numberTwo, operation) => {
-
+  while (operation === '-') {
+    return new Big(numberOne).minus(new Big(numberTwo));
+  }
 };
 
 export default operate;
