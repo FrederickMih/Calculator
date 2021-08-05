b6e18062a37369254b4e9184caeedd8478fdf01e
FrederickMih <fredtimermih@gmail.com>
2021-08-05 02:12:44 +0100

    Modify App component to display the result of the calculations

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Display from './Display';
-import ButtonPanel from './ButtonPanel';
-import './App.css';
+// import ButtonPanel from './ButtonPanel';
+import '../App.css';
 
 function App() {
   return (
-    <div>
-      <p>This is my first calculator app in React</p>
-      <Display />
-      <ButtonPanel />
-    </div>
+    <>
+      <Display result="Calculations" />
+      {/* <ButtonPanel /> */}
+    </>
   );
 }
 
