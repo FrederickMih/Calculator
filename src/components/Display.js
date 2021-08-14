297cf06dc8d81467daafa5365f1a561c9aceb57e
FrederickMih <fredtimermih@gmail.com>
2021-08-14 03:53:01 +0100

    Modify the Display component to ES6 arrow function

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Display(props) {
+const Display = (props) => {
   const { result } = props;
   return (
     <div>
       <h2>{result}</h2>
     </div>
   );
-}
+};
 
 Display.propTypes = {
   result: PropTypes.string,
