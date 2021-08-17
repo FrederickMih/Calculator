6d4637e660496d47f12809a1b8f40f8ba98f504e
FrederickMih <fredtimermih@gmail.com>
2021-08-17 04:42:35 +0100

    Made Button width 25%

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../styles/Button.css';
 
 const Button = (props) => {
-  const { name } = props;
+  const { name, color } = props;
   const handleClick = (btnName) => props.clickHandler(btnName);
+  const styles = {
+    backgroundColor: color,
+  };
 
   return (
-    <button className="btn" type="button" onClick={() => handleClick(name)}>
+    <button
+      className={name === '0' ? 'button zero-button' : 'button'}
+      type="button"
+      style={styles}
+      onClick={() => handleClick(name)}
+    >
       {name}
     </button>
   );
 };
 
+Button.defaultProps = {
+  color: 'orange',
+};
+
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   clickHandler: PropTypes.func.isRequired,
+  color: PropTypes.string,
 };
 
 export default Button;
