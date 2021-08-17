8ed5c3647868a0730140d896b4e332e35c13ea69
FrederickMih <fredtimermih@gmail.com>
2021-08-17 15:09:08 +0100

    Modify quotes page

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import '../styles/Quotes.css';
 
 const Quote = () => (
-  <div
-
-  >
+  <div className="some-quotes">
+    <h3 className="quotes-h3">Quotes</h3>
     <p>
       Mathematics is not about numbers, equations, computations, or algorithms:
       it is about understanding. –William Paul Thurston
