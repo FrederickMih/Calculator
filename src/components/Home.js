fbd9a7f39894e82b48fa5668332aa10964fd213b
FrederickMih <fredtimermih@gmail.com>
2021-08-17 06:13:24 +0100

    Add Home and Quote component

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Home = () => {
+  <div
+    style={{
+      width: '75%',
+      margin: '0 auto',
+      padding: '4rem',
+      borderRadius: '3px',
+      boxShadow: 'rgba(100, 100, 111, 0.3,) 0 7px 29px 0;',
+    }}
+  >
+    <h3 className="home-page">Welcome</h3>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi illum, in
+      optio ut, eos recusandae doloribus officia cum animi vero culpa impedit
+      facere voluptatum, voluptatem error odio nemo harum magni magnam! Quis
+      exercitationem, ipsam, vero delectus minima mollitia eaque totam
+      perspiciatis vitae, asperiores ullam ipsum consequuntur? Soluta quam
+      ratione dolores!
+    </p>
+  </div>;
+};
+
+export default Home;
---
+++ src/components/Quote.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quote = () => (
+  <div>
+    <p>
+      This is not about numbers, equations, computations, or algorithm: it is
+      about understanding. -William Paul Thurston
+    </p>
+  </div>
+);
+
+export default Quote;
