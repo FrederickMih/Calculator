859a9b424021c215576fe26d52427c82cc49a765
FrederickMih <fredtimermih@gmail.com>
2021-08-17 14:58:47 +0100

    Modify the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,17 @@ import '../styles/Home.css';
 
 const Home = () => (
   <div className="home-page">
-    <h3 className="home-h3">Welcome</h3>
+    <h3 className="home-h3">Welcome!</h3>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi illum, in
+      optio ut, eos recusandae doloribus officia cum animi vero culpa impedit
+      facere voluptatum, voluptatem error odio nemo harum magni magnam! Quis
+      exercitationem, ipsam, vero delectus minima mollitia eaque totam
+      perspiciatis vitae, asperiores ullam ipsum consequuntur? Soluta quam
+      ratione dolores!
+    </p>
+    <br />
+    <br />
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi illum, in
       optio ut, eos recusandae doloribus officia cum animi vero culpa impedit
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,17 +2,11 @@ import React from 'react';
 
 const Quote = () => (
   <div
-    style={{
-      width: '75%',
-      margin: '0 auto',
-      padding: '4rem',
-      boxShadow: 'rgba(100, 100, 111, 0.2,), 0 7px 29px 0',
-      borderRadius: '3px',
-    }}
+
   >
     <p>
-      This is not about numbers, equations, computations, or algorithm: it is
-      about understanding. -William Paul Thurston
+      Mathematics is not about numbers, equations, computations, or algorithms:
+      it is about understanding. –William Paul Thurston
     </p>
   </div>
 );
