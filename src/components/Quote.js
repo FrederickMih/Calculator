c76c230bf3e62fd71c132cbf62bbfee3fe43dd41
FrederickMih <fredtimermih@gmail.com>
2021-08-17 06:23:11 +0100

    Add some inline styles in the Quote component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Quote = () => (
-  <div>
+  <div
+    style={{
+      width: '75%',
+      margin: '0 auto',
+      padding: '4rem',
+      boxShadow: 'rgba(100, 100, 111, 0.2,), 0 7px 29px 0',
+      borderRadius: '3px',
+    }}
+  >
     <p>
       This is not about numbers, equations, computations, or algorithm: it is
       about understanding. -William Paul Thurston
