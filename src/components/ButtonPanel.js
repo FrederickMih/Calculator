66ef9ab8084225d90a0260cc85418655fbf783c1
FrederickMih <fredtimermih@gmail.com>
2021-08-17 03:45:20 +0100

    Add classes ButtonPanel component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -7,37 +7,37 @@ const ButtonPanel = (props) => {
   // eslint-disable-next-line react/prop-types
   const handleClick = (btnName) => props.clickHandler(btnName);
   return (
-    <>
-      <div>
+    <div className="button-panel">
+      <div className="group-1 btn">
         <Button name="AC" clickHandler={handleClick} />
         <Button name="+/-" clickHandler={handleClick} />
         <Button name="%" clickHandler={handleClick} />
         <Button name="÷" clickHandler={handleClick} />
       </div>
-      <div>
+      <div className="group-2 btn">
         <Button name="7" clickHandler={handleClick} />
         <Button name="8" clickHandler={handleClick} />
         <Button name="9" clickHandler={handleClick} />
         <Button name="x" clickHandler={handleClick} />
       </div>
-      <div>
+      <div className="group-3 btn">
         <Button name="4" clickHandler={handleClick} />
         <Button name="5" clickHandler={handleClick} />
         <Button name="6" clickHandler={handleClick} />
         <Button name="-" clickHandler={handleClick} />
       </div>
-      <div>
+      <div className="group-4 btn">
         <Button name="1" clickHandler={handleClick} />
         <Button name="2" clickHandler={handleClick} />
         <Button name="3" clickHandler={handleClick} />
         <Button name="+" clickHandler={handleClick} />
       </div>
-      <div>
+      <div className="group-5 btn">
         <Button name="0" clickHandler={handleClick} />
         <Button name="." clickHandler={handleClick} />
         <Button name="=" clickHandler={handleClick} />
       </div>
-    </>
+    </div>
   );
 };
 
