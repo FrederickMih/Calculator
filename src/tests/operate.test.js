77ec05ec4e7cb9b3bf13b4032f6e7d2763b27cc9
FrederickMih <fredtimermih@gmail.com>
2021-08-18 06:39:27 +0100

    Add test for minus sign

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -2,9 +2,14 @@ import operate from '../logic/operate';
 
 describe('aritmetic operations', () => {
   it('should sum', () => {
-    expect(JSON.stringify(operate(5, 6, '+'))).toStrictEqual(JSON.stringify('11'));
+    expect(JSON.stringify(operate(5, 6, '+'))).toStrictEqual(
+      JSON.stringify('11')
+    );
   });
   it('calculate falsy sum', () => {
     expect(JSON.stringify(operate(3, 4, '+'))).not.toEqual(JSON.stringify('4'));
+  });
+  it('should subtract', () => {
+    expect(JSON.stringify(operate(10, -5, '-'))).toEqual(JSON.stringify('15'));
   });
 });
