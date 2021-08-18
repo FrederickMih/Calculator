410a69d3ebf6f7b4b1467092074f2f7efe68f66f
FrederickMih <fredtimermih@gmail.com>
2021-08-18 06:37:19 +0100

    Add test for falsy plus

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -4,4 +4,7 @@ describe('aritmetic operations', () => {
   it('should sum', () => {
     expect(JSON.stringify(operate(5, 6, '+'))).toStrictEqual(JSON.stringify('11'));
   });
+  it('calculate falsy sum', () => {
+    expect(JSON.stringify(operate(3, 4, '+'))).not.toEqual(JSON.stringify('4'));
+  });
 });
